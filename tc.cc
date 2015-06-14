dac9361a845cf6880b880fd9b77d15c9fbd18578
Scott Beamer <sbeamer@eecs.berkeley.edu>
2015-06-14 07:46:35 -0700

    including tc

---
+++ tc.cc
@@ -0,0 +1,88 @@
+#include <algorithm>
+#include <cinttypes>
+#include <iostream>
+
+#include "benchmark.h"
+#include "builder.h"
+#include "command_line.h"
+#include "graph.h"
+#include "pvector.h"
+
+
+using namespace std;
+
+
+// assumes neighborhoods are sorted, no self loops, no duplicate edges
+// counts triangles only once by ordering u>v>w
+size_t OrderedCount(const Graph &g) {
+  size_t total = 0;
+  #pragma omp parallel for reduction(+ : total) schedule(dynamic)
+  for (NodeID u=0; u < g.num_nodes(); u++) {
+    for (NodeID v : g.out_neigh(u)) {
+      if (v > u)
+        break;
+      auto it = g.out_neigh(u).begin();
+      for (NodeID w : g.out_neigh(v)) {
+        if (w > v)
+          break;
+        while (*it < w)
+          it++;
+        if (w == *it)
+          total++;
+      }
+    }
+  }
+  return total;
+}
+
+
+// relabels graph by degree before using ordered counting
+size_t DegreeOrderedCount(const Graph &g) {
+  Graph g_by_degree = Builder::RelabelByDegree(g);
+  return OrderedCount(g_by_degree);
+}
+
+
+// heuristic to see if sufficently dense power-law graph
+bool WorthRelabelling(const Graph &g) {
+  int64_t average_degree = g.num_edges() / g.num_nodes();
+  if (average_degree < 10)
+    return false;
+  SourcePicker<Graph> sp(g);
+  int64_t num_samples = min(1000l, g.num_nodes());
+  int64_t sample_total = 0;
+  pvector<int64_t> samples(num_samples);
+  for (int64_t trial=0; trial < num_samples; trial++) {
+    samples[trial] = g.out_degree(sp.PickNext());
+    sample_total += samples[trial];
+  }
+  sort(samples.begin(), samples.end());
+  double sample_average = static_cast<double>(sample_total) / num_samples;
+  double sample_median = samples[num_samples/2];
+  return sample_average / 2 > sample_median;
+}
+
+
+// uses heuristic to see if worth relabeling
+size_t Hybrid(const Graph &g) {
+  if (WorthRelabelling(g))
+    return DegreeOrderedCount(g);
+  else
+    return OrderedCount(g);
+}
+
+
+void PrintTriangleStats(const Graph &g, size_t total_triangles) {
+  cout << total_triangles << " triangles" << endl;
+}
+
+
+int main(int argc, char* argv[]) {
+  CLApp cli(argc, argv, "triangle count");
+  if (!cli.ParseArgs())
+    return -1;
+  Builder b(cli);
+  Graph g = b.MakeGraph();
+  BenchmarkFunc(cli, g, Hybrid, PrintTriangleStats);
+  return 0;
+}
