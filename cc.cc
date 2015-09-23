a28828537fbf75f49b7060938643ebbcc8d76cea
Scott Beamer <sbeamer@eecs.berkeley.edu>
2015-09-23 09:04:00 -0700

    property-based cc verifier

--- cc.cc
+++ cc.cc
@@ -8,6 +8,7 @@
 #include <vector>
 
 #include "benchmark.h"
+#include "bitmap.h"
 #include "builder.h"
 #include "command_line.h"
 #include "graph.h"
@@ -66,6 +67,7 @@ pvector<NodeID> ShiloachVishkin(const Graph &g) {
   return comp;
 }
 
+
 void PrintCompStats(const Graph &g, const pvector<NodeID> &comp) {
   cout << endl;
   unordered_map<NodeID, NodeID> count;
@@ -85,12 +87,59 @@ void PrintCompStats(const Graph &g, const pvector<NodeID> &comp) {
 }
 
 
+// Verifies CC result by performing a BFS from a vertex in each component
+// - Asserts search does not reach a vertex with a different component label
+// - If the graph is directed, it performs the search as if it was undirected
+// - Asserts every vertex is visited (degree-0 vertex should have own label)
+bool CCVerifier(const Graph &g, const pvector<NodeID> &comp) {
+  unordered_map<NodeID, NodeID> label_to_source;
+  for (NodeID n=0; n < g.num_nodes(); n++)
+    label_to_source[comp[n]] = n;
+  Bitmap visited(g.num_nodes());
+  visited.reset();
+  vector<NodeID> frontier;
+  frontier.reserve(g.num_nodes());
+  for (auto label_source_pair : label_to_source) {
+    NodeID curr_label = label_source_pair.first;
+    NodeID source = label_source_pair.second;
+    frontier.clear();
+    frontier.push_back(source);
+    visited.set_bit(source);
+    for (auto it = frontier.begin(); it != frontier.end(); it++) {
+      NodeID u = *it;
+      for (NodeID v : g.out_neigh(u)) {
+        if (comp[v] != curr_label)
+          return false;
+        if (!visited.get_bit(v)) {
+          visited.set_bit(v);
+          frontier.push_back(v);
+        }
+      }
+      if (g.directed()) {
+        for (NodeID v : g.in_neigh(u)) {
+          if (comp[v] != curr_label)
+            return false;
+          if (!visited.get_bit(v)) {
+            visited.set_bit(v);
+            frontier.push_back(v);
+          }
+        }
+      }
+    }
+  }
+  for (NodeID n=0; n < g.num_nodes(); n++)
+    if (!visited.get_bit(n))
+      return false;
+  return true;
+}
+
+
 int main(int argc, char* argv[]) {
   CLApp cli(argc, argv, "connected-components");
   if (!cli.ParseArgs())
     return -1;
   Builder b(cli);
   Graph g = b.MakeGraph();
-  BenchmarkKernel(cli, g, ShiloachVishkin, PrintCompStats, VerifyUnimplemented);
+  BenchmarkKernel(cli, g, ShiloachVishkin, PrintCompStats, CCVerifier);
   return 0;
 }
