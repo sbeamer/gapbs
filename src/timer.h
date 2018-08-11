d69b811c04aeffefccff7a446b8281812b272125
michaelsutton <msutton@cs.huji.ac.il>
2018-08-12 01:49:23 +0300

    [core] timer: fixed chrono usage, removed ifdefs and the linux gettimeofday-based timer

--- src/timer.h
+++ src/timer.h
@@ -4,83 +4,44 @@
 #ifndef TIMER_H_
 #define TIMER_H_
 
+#include <chrono>
+
 
 /*
 GAP Benchmark Suite
 Class:  Timer
 Authors: Scott Beamer, Michael Sutton
 
-Simple timer that wraps gettimeofday or uses chrono for Windows
+Simple timer that wraps std::chrono
 */
 
-#if defined _WIN32
-
-#include <chrono>
-
 class Timer {
  public:
   Timer() {}
 
   void Start() {
-      t2 = t1 = std::chrono::high_resolution_clock::now();
+    elapsed_time_ = start_time_ = std::chrono::high_resolution_clock::now();
   }
 
   void Stop() {
-      t2 = std::chrono::high_resolution_clock::now();
+    elapsed_time_ = std::chrono::high_resolution_clock::now();
   }
 
   double Seconds() const {
-    return std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() / 1000000.0;
+    return std::chrono::duration_cast<std::chrono::duration<double>>(elapsed_time_ - start_time_).count();
   }
 
   double Millisecs() const {
-    return std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() / 1000.0;
+    return std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(elapsed_time_ - start_time_).count();
   }
 
   double Microsecs() const {
-    return std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count() / 1.0;
+    return std::chrono::duration_cast<std::chrono::duration<double, std::micro>>(elapsed_time_ - start_time_).count();
   }
 
  private:
-  std::chrono::high_resolution_clock::time_point t1, t2;
+  std::chrono::high_resolution_clock::time_point start_time_, elapsed_time_;
 };
-
-#else
-
-#include <sys/time.h>
-
-class Timer {
- public:
-  Timer() {}
-
-  void Start() {
-    gettimeofday(&start_time_, NULL);
-  }
-
-  void Stop() {
-    gettimeofday(&elapsed_time_, NULL);
-    elapsed_time_.tv_sec  -= start_time_.tv_sec;
-    elapsed_time_.tv_usec -= start_time_.tv_usec;
-  }
-
-  double Seconds() const {
-    return elapsed_time_.tv_sec + elapsed_time_.tv_usec/1e6;
-  }
-
-  double Millisecs() const {
-    return 1000*elapsed_time_.tv_sec + elapsed_time_.tv_usec/1000;
-  }
-
-  double Microsecs() const {
-    return 1e6*elapsed_time_.tv_sec + elapsed_time_.tv_usec;
-  }
-
- private:
-  struct timeval start_time_;
-  struct timeval elapsed_time_;
-};
-
-#endif
 
 // Times op's execution using the timer t
 #define TIME_OP(t, op) { t.Start(); (op); t.Stop(); }
