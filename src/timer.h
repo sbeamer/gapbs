12ceb47d40f4b65340c4b7072d737770b89eeaae
michaelsutton <msutton@cs.huji.ac.il>
2018-08-12 02:47:16 +0300

    [core] chrono-based timer

--- src/timer.h
+++ src/timer.h
@@ -4,47 +4,43 @@
 #ifndef TIMER_H_
 #define TIMER_H_
 
-#include <sys/time.h>
+#include <chrono>
 
 
 /*
 GAP Benchmark Suite
 Class:  Timer
-Author: Scott Beamer
+Authors: Scott Beamer, Michael Sutton
 
-Simple timer that wraps gettimeofday
+Simple timer that wraps std::chrono
 */
-
 
 class Timer {
  public:
   Timer() {}
 
   void Start() {
-    gettimeofday(&start_time_, NULL);
+    elapsed_time_ = start_time_ = std::chrono::high_resolution_clock::now();
   }
 
   void Stop() {
-    gettimeofday(&elapsed_time_, NULL);
-    elapsed_time_.tv_sec  -= start_time_.tv_sec;
-    elapsed_time_.tv_usec -= start_time_.tv_usec;
+    elapsed_time_ = std::chrono::high_resolution_clock::now();
   }
 
   double Seconds() const {
-    return elapsed_time_.tv_sec + elapsed_time_.tv_usec/1e6;
+    return std::chrono::duration_cast<std::chrono::duration<double>>(elapsed_time_ - start_time_).count();
   }
 
   double Millisecs() const {
-    return 1000*elapsed_time_.tv_sec + elapsed_time_.tv_usec/1000;
+    return std::chrono::duration_cast<std::chrono::duration<double, std::milli>>(elapsed_time_ - start_time_).count();
   }
 
   double Microsecs() const {
-    return 1e6*elapsed_time_.tv_sec + elapsed_time_.tv_usec;
+    return std::chrono::duration_cast<std::chrono::duration<double, std::micro>>(elapsed_time_ - start_time_).count();
   }
 
  private:
-  struct timeval start_time_;
-  struct timeval elapsed_time_;
+  std::chrono::high_resolution_clock::time_point start_time_, elapsed_time_;
 };
 
 // Times op's execution using the timer t
