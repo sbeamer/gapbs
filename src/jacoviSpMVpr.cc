69aea27a268f6b393f398763148ab915e8ea049c
Connor Masterson <ccmasterson28@gmail.com>
2021-05-18 13:54:47 -0700

    Gauss-Seidel Style PR (#28)
    * pr.cc now uses Gauss-Seidel style pr
    
    * larger loop chunk size in pr
    
    * remove unused library
    
    Co-authored-by: Connor Curran Masterson <ccmaster@keylime.soe.ucsc.edu>

--- src/jacoviSpMVpr.cc
+++ src/jacoviSpMVpr.cc
@@ -11,6 +11,7 @@
 #include "graph.h"
 #include "pvector.h"
 
+
 /*
 GAP Benchmark Suite
 Kernel: PageRank (PR)
@@ -30,19 +31,18 @@ using namespace std;
 typedef float ScoreT;
 const float kDamp = 0.85;
 
-
-pvector<ScoreT> PageRankPullGS(const Graph &g, int max_iters,
+pvector<ScoreT> PageRankPull(const Graph &g, int max_iters,
                              double epsilon = 0) {
   const ScoreT init_score = 1.0f / g.num_nodes();
   const ScoreT base_score = (1.0f - kDamp) / g.num_nodes();
   pvector<ScoreT> scores(g.num_nodes(), init_score);
   pvector<ScoreT> outgoing_contrib(g.num_nodes());
-  #pragma omp parallel for
-  for (NodeID n=0; n < g.num_nodes(); n++)
-    outgoing_contrib[n] = init_score / g.out_degree(n);
   for (int iter=0; iter < max_iters; iter++) {
     double error = 0;
-    #pragma omp parallel for reduction(+ : error) schedule(dynamic, 16384)
+    #pragma omp parallel for
+    for (NodeID n=0; n < g.num_nodes(); n++)
+      outgoing_contrib[n] = scores[n] / g.out_degree(n);
+    #pragma omp parallel for reduction(+ : error) schedule(dynamic, 64)
     for (NodeID u=0; u < g.num_nodes(); u++) {
       ScoreT incoming_total = 0;
       for (NodeID v : g.in_neigh(u))
@@ -50,7 +50,6 @@ pvector<ScoreT> PageRankPullGS(const Graph &g, int max_iters,
       ScoreT old_score = scores[u];
       scores[u] = base_score + kDamp * incoming_total;
       error += fabs(scores[u] - old_score);
-      outgoing_contrib[u] = scores[u] / g.out_degree(u);
     }
     printf(" %2d    %lf\n", iter, error);
     if (error < epsilon)
@@ -101,7 +100,7 @@ int main(int argc, char* argv[]) {
   Builder b(cli);
   Graph g = b.MakeGraph();
   auto PRBound = [&cli] (const Graph &g) {
-    return PageRankPullGS(g, cli.max_iters(), cli.tolerance());
+    return PageRankPull(g, cli.max_iters(), cli.tolerance());
   };
   auto VerifierBound = [&cli] (const Graph &g, const pvector<ScoreT> &scores) {
     return PRVerifier(g, scores, cli.tolerance());
