3100ea6d55bd5f995a7dc2538c07e0b53fa56e06
Scott Beamer <sbeamer@ucsc.edu>
2021-06-14 11:43:07 -0700

    [tc] encourage use of parallel sort

--- src/tc.cc
+++ src/tc.cc
@@ -1,6 +1,9 @@
 // Copyright (c) 2015, The Regents of the University of California (Regents)
 // See LICENSE.txt for license details
 
+// Encourage use of gcc's parallel algorithms (for sort for relabeling)
+#define _GLIBCXX_PARALLEL
+
 #include <algorithm>
 #include <cinttypes>
 #include <iostream>
